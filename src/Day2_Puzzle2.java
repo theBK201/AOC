b39b72a4f7772fb9807f12659609c27882fb1f37
bk201 <bk201.dev@gmail.com>
2021-12-02 18:08:04 +0100

    day 2 done

--- src/Day2_Puzzle2.java
+++ src/Day2_Puzzle2.java
@@ -2,10 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
 
-public class Day2 {
+public class Day2_Puzzle2 {
     public static void main(String[] args) {
         int hPos = 0;
         int depth = 0;
+        int aim = 0;
         int multiplication = 0;
 
         try(BufferedReader br = new BufferedReader(new FileReader("/home/bk201/Coding/blobs/src/adventInput.txt"))){
@@ -13,16 +14,15 @@ public class Day2 {
             while ((line=br.readLine()) != null){
                 String[] result = line.split(" ");
                 if(result[0].equals("forward")){
-                    hPos = hPos + Integer.parseInt(result[1]);
+                    hPos  += Integer.parseInt(result[1]);
+                    depth += aim * Integer.parseInt(result[1]);
                 }
                 if(result[0].equals("down")){
-                   depth += Integer.parseInt(result[1]);
+                    aim += Integer.parseInt(result[1]);
                 }
                 if(result[0].equals("up")){
-                    depth -= Integer.parseInt(result[1]);
+                    aim -= Integer.parseInt(result[1]);
                 }
-
-                System.out.println(Arrays.toString(result));
             }
             System.out.println(multiplication = hPos * depth);
         }catch (Exception e){
